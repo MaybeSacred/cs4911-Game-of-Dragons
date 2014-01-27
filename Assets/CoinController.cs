fd2be4f8b11eaeab795fc49f75a1235ddfaec642
Jon <jtyson3@gatech.edu>
2014-01-27 14:45:08 -0500

    Coins
    blah

---
+++ Assets/CoinController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour {
+	private Quaternion rotationQuaternion;
+	public float rotationSpeed;
+	void Start () {
+		Debug.Log("hi");
+		Debug.Log("hi");
+		Debug.Log("hi");
+		Debug.Log("hi");
+		Debug.Log("hi");Debug.Log("hi");
+		
+	}
+	
+	void Update () {
+		transform.rotation *= new Quaternion(0, Mathf.Sin(rotationSpeed*Time.deltaTime), 0, Mathf.Cos(rotationSpeed*Time.deltaTime));
+	}
+}
