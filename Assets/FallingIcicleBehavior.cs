99e56b81d631c4b1a96693a4c103cf11cb5da4d0
Jon <jtyson3@gatech.edu>
2014-01-29 01:18:29 -0500

    Additional Changes
    Added icicles and scripting

---
+++ Assets/FallingIcicleBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingIcicleBehavior : MonoBehaviour {
+	public float dropTimeAfterTriggered;
+	private float dropTimer;
+	private Vector3 initialPosition;
+	public float shakingAmplitude;
+	public float shakingSpeed;
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	void Update () {
+		if(dropTimer > 0 && dropTimer < dropTimeAfterTriggered)
+		{
+			transform.position = initialPosition+new Vector3(Mathf.PerlinNoise(dropTimer*shakingSpeed, 0), 0, Mathf.PerlinNoise(0, shakingSpeed*dropTimer))*shakingAmplitude;
+			dropTimer += Time.deltaTime;
+			if(dropTimer >= dropTimeAfterTriggered)
+			{
+				rigidbody.isKinematic = false;
+			}
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag.Equals("Player"))
+		{
+			dropTimer += Time.deltaTime;
+		}
+	}
+}
