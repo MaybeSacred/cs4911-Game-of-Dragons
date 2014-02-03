aa7c14212cbe2e83555efa9d3fab0e7ecf16c017
Jon <jtyson3@gatech.edu>
2014-02-02 23:50:16 -0500

    Added some models

---
+++ Assets/HellmetBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HellmetBehavior : MonoBehaviour {
+	public float bouncyness;
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+	}
+}
