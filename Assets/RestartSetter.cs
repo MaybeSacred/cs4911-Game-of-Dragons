cf9025e15435a82321bd8595a60256073ac8bd6d
Jon <jtyson3@gatech.edu>
2014-02-19 14:33:29 -0500

    Added restartSetter script

---
+++ Assets/RestartSetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartSetter : MonoBehaviour {
+	public Vector3 desiredPosition;
+	public Vector3 desiredRotation;
+	public int checkPointNumber;
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag.Equals("Player"))
+		{
+			WorldScript.player.SetLastCheckpoint(desiredPosition + transform.position, desiredRotation, checkPointNumber);
+		}
+	}
+}
