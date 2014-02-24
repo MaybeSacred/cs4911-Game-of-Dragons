322a973faf6e25e1432034dc929230611748ec7b
khkurzeja <kkurzeja3@gatech.edu>
2014-02-24 15:18:04 -0500

    Hellmet Resettable and Added Checkpoint

---
+++ Assets/CheckpointBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointBehaviour : GameBehaviour 
+{
+
+	public Transform letterC;
+	private float letterCRotateSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		base.Start ();
+
+		letterCRotateSpeed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		letterC.Rotate(0, letterCRotateSpeed, 0);
+	}
+}
