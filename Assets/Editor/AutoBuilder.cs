fc5fce73046b9b2a87182d33a7d1eae5ec7403f9
Jon <jtyson3@gatech.edu>
2014-03-05 21:38:00 -0500

    Added autobuilder, but it seems to require unity pro

---
+++ Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class AutoBuilder : MonoBehaviour {
+
+	void Start () {
+
+	}
+	
+	void Update () {
+	
+	}
+	static void PerformBuild ()
+	{
+		
+		string[] scenes = {"Scene0.unity"};
+		//BuildPipeline.BuildPlayer(scenes,"win.exe",BuildTarget.StandaloneWindows,BuildOptions.None);
+		//BuildPipeline.BuildPlayer(scenes,"osx.app",BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes,"WebPlayer.unity3d",BuildTarget.WebPlayerStreamed,BuildOptions.Development);
+		scenes = null;
+		
+	}
+}
