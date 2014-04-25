3ca0fb0005471575909d0bea11816a1fc1a8c415
jillsthebest <jill.c.russek@gmail.com>
2014-04-25 15:39:57 -0400

    last

--- build/Autobuild.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Unity\Editor\Unity.exe" -batchMode -quit -nographics -projectPath C:\Users\Jon\Documents\GitHub\cs4911-Game-of-Dragons -executeMethod AutoBuilder.PerformBuild
-PAUSE
